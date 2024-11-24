339e70d28ff5ea1f9c376d68dbd6aa2f4dceb3e9
yassein-97 <ahmedyassein.97@gmail.com>
2024-11-24 15:32:26 +0200

    img1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import demo from '../../assets/images/img-5.jpg'
 
 
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <>
       <h1>hello from Home</h1>
+      <img src={demo} alt="" />
     </>
   );
 }
