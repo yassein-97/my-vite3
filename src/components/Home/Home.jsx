b3b15808d6e3a9dbf00607f0e002f3958d655229
yassein-97 <ahmedyassein.97@gmail.com>
2024-11-24 15:45:52 +0200

    mo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <h1>hello from Home</h1>
-      <img src='./src/assets/images/img-5.jpg' alt="" />
+      <img src='https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSNDa7RDY4jijVo7-2duRBbo3Ggoj0Jkoxyha0b4z29nQ6dqGJCW_KZTiA468JDB0cv8WD1Sd9hAgQ4Fmw' alt="" />
     </>
   );
 }
