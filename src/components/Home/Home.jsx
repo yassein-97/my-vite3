da911081b85bdf36bd62949f096a63e740a739f6
yassein-97 <ahmedyassein.97@gmail.com>
2024-11-26 16:51:27 +0200

    new img

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-// import demo from '../../assets/images/img-5.jpg'
+import test from '../../assets/images/logo.png'
+import demo from '../../assets/images/react.svg'
+import done from '../../assets/images/img-5.jpg'
 
 
 
@@ -8,7 +10,9 @@ export default function Home() {
   return (
     <>
       <h1>hello from Home</h1>
-      <img src='https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSNDa7RDY4jijVo7-2duRBbo3Ggoj0Jkoxyha0b4z29nQ6dqGJCW_KZTiA468JDB0cv8WD1Sd9hAgQ4Fmw' alt="" />
-    </>
+      <img src={test} alt="" />
+      <img src={demo} alt="" />
+      <img src={done} alt="" />
+     </>
   );
 }
