812add58c52a137e5c18756a1f7bb6c0c9707195
yassein-97 <ahmedyassein.97@gmail.com>
2024-11-26 17:01:23 +0200

    avout

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import nam from '../../assets/images/logo'
+
 export default function About() {
   return (
     <>
       <h1>hello from About</h1>
+      <img src={nam} alt="" />
     </>
   );
 }
